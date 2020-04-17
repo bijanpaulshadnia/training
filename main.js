eb31447c40b8741d2d32d775934700a58a2a14a7
Bijan <bijanpaulshadnia@gmail.com>
2020-04-17 16:18:48 -0500

    do test1

---
+++ main.js
@@ -0,0 +1,12 @@
+var Tesla = 0;
+var CarsToyota = 1;
+var CarsBMW = 2;
+var CarsMiniCooper = 3;
+var Cars;
+(function (Cars) {
+    Cars[Cars["Tesla"] = 0] = "Tesla";
+    Cars[Cars["Toyota"] = 1] = "Toyota";
+    Cars[Cars["BMW"] = 2] = "BMW";
+    Cars[Cars["MiniCooper"] = 3] = "MiniCooper";
+})(Cars || (Cars = {}));
+;
---
+++ main.ts
@@ -0,0 +1,6 @@
+const Tesla=0;
+const CarsToyota=1; 
+const CarsBMW=2;
+const CarsMiniCooper=3;
+
+enum Cars {Tesla = 0, Toyota=1, BMW=2, MiniCooper=3};
